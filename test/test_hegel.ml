9ca0662e51251752eed2ef37cc2a7be4c9435977
Liam DeVoe <orionldevoe@gmail.com>
2026-03-03 23:28:06 -0500

    remove bad test

--- test/test_hegel.ml
+++ test/test_hegel.ml
@@ -1,10 +1,6 @@
-let test_version () =
-  Alcotest.(check string) "version string" "0.1.0" Hegel.version
-
 let () =
   Alcotest.run "hegel"
     [
-      ("version", [ Alcotest.test_case "returns version" `Quick test_version ]);
       ("protocol", Test_protocol.tests);
       ("cbor_helpers", Test_cbor_helpers.tests);
       ("connection", Test_connection.tests);
