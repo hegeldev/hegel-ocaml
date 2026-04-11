643cbdb3d17e872e7319cbd2df61bfe6f8128fdf
Liam DeVoe <orionldevoe@gmail.com>
2026-04-11 17:17:52 -0400

    remove showcase

--- test/test_hegel.ml
+++ test/test_hegel.ml
@@ -9,7 +9,6 @@ let () =
       ("generators_primitives", Test_generators_primitives.tests);
       ("generators_collections", Test_generators_collections.tests);
       ("generators_combinators", Test_generators_combinators.tests);
-      ("showcase", Test_showcase.tests);
       ("conformance_helpers", Test_conformance_helpers.tests);
       ("derive", Test_derive.tests);
     ]
